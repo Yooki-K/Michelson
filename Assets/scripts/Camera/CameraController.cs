2df06dfdc0693539b3fccd494e0c0d1ba7a430e4
ky_sfms <1486147017@qq.com>
2021-10-30 19:43:18 +0800

    第二次提交，合并作品

---
+++ Assets/scripts/Camera/CameraController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraController : MonoBehaviour
+{
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float dx = Input.GetAxis("Mouse X");
+        float dy = Input.GetAxis("Mouse Y");
+
+
+        
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                Debug.Log(hitInfo.transform.name);
+            }
+        }
+        // 旋转 鼠标右键
+        if (Input.GetMouseButton(1)) 
+        {
+            if (Mathf.Abs(dx) > 0 || Mathf.Abs(dy) > 0)
+            {
+                // 获取摄像机欧拉角
+                Vector3 angles = transform.rotation.eulerAngles;
+                
+                // 欧拉角表示按照坐标顺序旋转，比如angles.x=30，表示按x轴旋转30°，dy改变引起x轴的变化
+                angles.x = Mathf.Repeat(angles.x + 180f, 360f) - 180f;
+                angles.y += dx;
+                angles.x -= dy;
+                // 设置摄像头旋转
+                Quaternion rotation = Quaternion.identity;
+                rotation.eulerAngles = new Vector3(0, angles.y, 0);
+                transform.rotation = rotation;
+            }
+        }
+        //移动
+        //空格键抬升高度
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed  * Time.deltaTime, transform.position.z);
+        }
+        //shift键降低高度
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - speed  * Time.deltaTime, transform.position.z);
+        }
+        //w键前进
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+        //s键后退
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+        }
+        //a键后退
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        //d键后退
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+    }
+}
+
---
+++ Assets/scripts/Spain/SpainController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SpainController : MonoBehaviour
+{
+    public float speed = 5;
+    private float angle = 0f;
+    public Text txt;
+    public DateTime LastTime ;
+    public bool IsTextActive = false;
+    private String ObjectName;
+
+
+    //click部分
+    //begin
+    public bool IsClick = false;
+    public bool IsDown = false;
+    public Color color;
+    public Color ActiveColor = Color.red;
+
+    public void OnMouseDown()
+    {
+        this.IsDown = true;
+    }
+
+    public void OnMouseUp()
+    {
+        if (IsDown)
+        {
+            if (GetComponent<MeshRenderer>().material.color == color)
+            {
+                GetComponent<MeshRenderer>().material.color = ActiveColor;
+                IsClick = true;
+                SetText(ObjectName + "角度为：" + angle.ToString());
+            }
+            else
+            {
+                GetComponent<MeshRenderer>().material.color = color;
+                IsClick = false;
+            }
+
+            IsDown = false;
+        }
+
+
+    }
+    //end
+    public void JudgeIsArrival()
+    {
+        if (txt.text == "" || LastTime == null || !IsTextActive) return;
+        TimeSpan ts = DateTime.Now - LastTime;
+        if (Convert.ToInt64(ts.TotalSeconds) > 10)
+        {
+            txt.text = "";
+            IsTextActive = false;
+        }
+        return;
+    }
+
+
+
+    public void SetText(String text)
+    {
+        txt.text = text;
+        LastTime = DateTime.Now;
+        IsTextActive = true;
+        Debug.Log(text);
+    }
+
+    void Start()
+    {
+        color = GetComponent<MeshRenderer>().material.color;
+        switch (gameObject.name)
+        {
+            case "Spain_large":
+                ObjectName = "粗螺旋";
+                break;
+            case "Spain_small":
+                ObjectName = "细螺旋";
+                break;
+            default:
+                ObjectName = gameObject.name;
+                break;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        JudgeIsArrival();
+        if (IsClick)
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+                angle -= speed * Time.deltaTime;
+                this.transform.Rotate(new Vector3(0, 0, -speed * Time.deltaTime));
+                SetText(ObjectName + "角度为：" + angle.ToString());
+
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                angle += speed * Time.deltaTime;
+                this.transform.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
+                SetText(ObjectName + "角度为：" + angle.ToString());
+            }
+            //if (Input.GetAxis("Mouse ScrollWheel") != 0)
+            //{
+
+            //    //鼠标滚动滑轮 值就会变化
+            //    if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            //    {
+            //        Debug.Log("-1");
+            //        this.transform.Rotate(new Vector3(0, 0, -speed * Time.deltaTime));
+            //    }
+            //    if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            //    {
+            //        Debug.Log("1");
+            //        this.transform.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
+            //    }
+            //}
+        }
+        
+
+    }
+
+
+
+
+
+
+}
